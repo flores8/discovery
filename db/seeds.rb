98f614108d76ba837bdfc1ce3a243e87fae2e692
flores8 <flores.lauralee@gmail.com>
2014-06-21 16:23:53 -0700

    Finished seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Discovery::Question.create(:question => 'When the phone rings do you', :option_1
 #2
 Discovery::Question.create(:question => 'Waiting in line, do you often', :option_1 => 'chat with others', :option_2 => 'stick to business', :personality_type => "Extravert/Introvert")
 #3
-Discovery::Question.create(:question => 'At a party, do you', :option_1 => 'interact iwth many, even strangers', :option_2 => 'interact iwth a few friends', :personality_type => "Extravert/Introvert")
+Discovery::Question.create(:question => 'At a party, do you', :option_1 => 'interact with many, even strangers', :option_2 => 'interact iwth a few friends', :personality_type => "Extravert/Introvert")
 #4
 Discovery::Question.create(:question => 'Does interacting with strangers', :option_1 => 'energize you', :option_2 => 'tax your reserves', :personality_type => "Extravert/Introvert")
 #5
@@ -63,4 +63,89 @@ Discovery::Question.create(:question => 'In stories do you prefer', :option_1 =>
 Discovery::Question.create(:question => 'Is it easier for you to', :option_1 => 'put others to good use', :option_2 => 'identify with others', :personality_type => "Sensory/Intuitive")
 
 
+# Thinking/Feeling
+#1
+Discovery::Question.create(:question => 'With people are you usually more', :option_1 => 'firm', :option_2 => 'gentle', :personality_type => "Thinking/Feeling")
+#2
+Discovery::Question.create(:question => 'Are you more comfortable in making', :option_1 => 'critical judgments', :option_2 => 'value judgements', :personality_type => "Thinking/Feeling")
+#3
+Discovery::Question.create(:question => 'In making up your mind are you more likely to go by', :option_1 => 'data', :option_2 => 'desires', :personality_type => "Thinking/Feeling")
+#4
+Discovery::Question.create(:question => 'In sizing up others do you tend to be', :option_1 => 'objective and impersonal', :option_2 => 'friendly and personal', :personality_type => "Thinking/Feeling")
+#5
+Discovery::Question.create(:question => 'Which appeals to you more', :option_1 => 'consistency of thought', :option_2 => 'harmonious relationships', :personality_type => "Thinking/Feeling")
+#6
+Discovery::Question.create(:question => 'If you must disappoint someone are you usually', :option_1 => 'frank and straightforward', :option_2 => 'warm and considerate', :personality_type => "Thinking/Feeling")
+#7
+Discovery::Question.create(:question => 'In a heated discussion, do you', :option_1 => 'stick to your guns', :option_2 => 'look for common ground', :personality_type => "Thinking/Feeling")
+#8
+Discovery::Question.create(:question => 'Is it better to be', :option_1 => 'just', :option_2 => 'merciful', :personality_type => "Thinking/Feeling")
+#9
+Discovery::Question.create(:question => 'When in charge of others do you tend to be', :option_1 => 'firm and unbending', :option_2 => 'forgiving and lenient', :personality_type => "Thinking/Feeling")
+#10
+Discovery::Question.create(:question => 'Are you more often', :option_1 => 'a cool-headed person', :option_2 => 'a warm-hearted person', :personality_type => "Thinking/Feeling")
+#11
+Discovery::Question.create(:question => 'Which is more of a compliment:', :option_1 => '\"There\'s a logical person\"', :option_2 => '\"There\'s a sentimental person\"', :personality_type => "Thinking/Feeling")
+#12
+Discovery::Question.create(:question => 'Which rules you more', :option_1 => 'your thoughts', :option_2 => 'your feelings', :personality_type => "Thinking/Feeling")
+#13
+Discovery::Question.create(:question => 'Is it worse to be', :option_1 => 'a softy', :option_2 => 'hard-nosed', :personality_type => "Thinking/Feeling")
+#14
+Discovery::Question.create(:question => 'In trying circumstances are you sometimes', :option_1 => 'too unsympathetic', :option_2 => 'too sympathetic', :personality_type => "Thinking/Feeling")
+#15
+Discovery::Question.create(:question => 'Do you think of yourself as a', :option_1 => 'tough-minded person', :option_2 => 'tender-hearted person', :personality_type => "Thinking/Feeling")
+#16
+Discovery::Question.create(:question => 'Do you value in yourself more that you are', :option_1 => 'reasonable', :option_2 => 'devoted', :personality_type => "Thinking/Feeling")
+#17
+Discovery::Question.create(:question => 'Which seems the greater fault', :option_1 => 'to be too compassionate', :option_2 => 'to be too dispassionate', :personality_type => "Thinking/Feeling")
+#18
+Discovery::Question.create(:question => 'Are you swayed more by', :option_1 => 'convincing evidence', :option_2 => 'a touching appeal', :personality_type => "Thinking/Feeling")
+#19
+Discovery::Question.create(:question => 'Which do you wish more for yourself', :option_1 => 'strength of will', :option_2 => 'strength of emotion', :personality_type => "Thinking/Feeling")
+#20
+Discovery::Question.create(:question => 'Do you see yourself as basically', :option_1 => 'thick-skinned', :option_2 => 'thin-skinned', :personality_type => "Thinking/Feeling")
+
+
+
+# Judging/Perceiving
+#1
+Discovery::Question.create(:question => 'Is clutter in the workplace something you', :option_1 => 'take time to straighten up', :option_2 => 'tolerate pretty well', :personality_type => "Judging/Perceiving")
+#2
+Discovery::Question.create(:question => 'Is it your way to', :option_1 => 'make up your mind quickly', :option_2 => 'pick and choose at some length', :personality_type => "Judging/Perceiving")
+#3
+Discovery::Question.create(:question => 'Do you prefer contracts to be', :option_1 => 'signed, sealed, and delivered', :option_2 => 'settled on a handshake', :personality_type => "Judging/Perceiving")
+#4
+Discovery::Question.create(:question => 'Are you more satisfied having', :option_1 => 'a finished product', :option_2 => 'work in progress', :personality_type => "Judging/Perceiving")
+#5
+Discovery::Question.create(:question => 'On the job do you want your activities', :option_1 => 'scheduled', :option_2 => 'unscheduled', :personality_type => "Judging/Perceiving")
+#6
+Discovery::Question.create(:question => 'Do you more often prefer', :option_1 => 'final, unalterable statements', :option_2 => 'tentative, preliminary statements', :personality_type => "Judging/Perceiving")
+#7
+Discovery::Question.create(:question => 'At work, is it more natural for you to', :option_1 => 'point out mistakes', :option_2 => 'try to please others', :personality_type => "Judging/Perceiving")
+#8
+Discovery::Question.create(:question => 'Are you more comfortable', :option_1 => 'after a decision', :option_2 => 'before a decision', :personality_type => "Judging/Perceiving")
+#9
+Discovery::Question.create(:question => 'Are you more prone to', :option_1 => 'nailing things down', :option_2 => 'exploring the possibilities', :personality_type => "Judging/Perceiving")
+#10
+Discovery::Question.create(:question => 'In most situations are you more', :option_1 => 'deliberate', :option_2 => 'spontaneous', :personality_type => "Judging/Perceiving")
+#11
+Discovery::Question.create(:question => 'When finishing a job, do you like to', :option_1 => 'tie up all the loose ends', :option_2 => 'more on to something else', :personality_type => "Judging/Perceiving")
+#12
+Discovery::Question.create(:question => 'Do you prefer to work', :option_1 => 'to deadlines', :option_2 => 'just whenever', :personality_type => "Judging/Perceiving")
+#13
+Discovery::Question.create(:question => 'Do you tend to choose', :option_1 => 'rather carefully', :option_2 => 'somewhat impulsively', :personality_type => "Judging/Perceiving")
+#14
+Discovery::Question.create(:question => 'Are you inclined to be more', :option_1 => 'hurried', :option_2 => 'leisurely', :personality_type => "Judging/Perceiving")
+#15
+Discovery::Question.create(:question => 'Do you usually want things', :option_1 => 'settled and decided', :option_2 => 'just penciled in', :personality_type => "Judging/Perceiving")
+#16
+Discovery::Question.create(:question => 'Would you say you are more', :option_1 => 'serious and determined', :option_2 => 'easy going', :personality_type => "Judging/Perceiving")
+#17
+Discovery::Question.create(:question => 'Do you feel better about', :option_1 => 'coming to closure', :option_2 => 'keeping your options open', :personality_type => "Judging/Perceiving")
+#18
+Discovery::Question.create(:question => 'Is it preferable mostly to', :option_1 => 'make sure things are arranged', :option_2 => 'just let things happen naturally', :personality_type => "Judging/Perceiving")
+#19
+Discovery::Question.create(:question => 'Do you tend to notice', :option_1 => 'disorderliness', :option_2 => 'opportunities for change', :personality_type => "Judging/Perceiving")
+#20
+Discovery::Question.create(:question => 'Are you more', :option_1 => 'routinized', :option_2 => 'whimsical', :personality_type => "Judging/Perceiving")
 
