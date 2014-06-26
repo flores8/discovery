2db276acb2be21cccd7cc386d849a50bea9efaaa
flores8 <flores.lauralee@gmail.com>
2014-06-26 11:37:15 -0700

    added initializer to run migrations from majorfinder

--- lib/discovery/engine.rb
+++ lib/discovery/engine.rb
@@ -1,5 +1,12 @@
 module Discovery
   class Engine < ::Rails::Engine
     isolate_namespace Discovery
+
+    initializer :append_migrations do |app|
+    	unless app.root.to_s.match root.to_s
+    		app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+    	end
+    end
+    
   end
 end
