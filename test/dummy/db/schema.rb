4d9993ce7babe830029a1b27a9de8e58eede1a8c
flores8 <flores.lauralee@gmail.com>
2015-01-26 17:43:17 -0700

    reset the db and reran it

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -13,54 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20150126155114) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-  end
-
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-
-  create_table "careers", :force => true do |t|
-    t.string   "name"
-    t.text     "glance"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "slug"
-    t.integer  "salary"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  add_index "careers", ["slug"], :name => "index_careers_on_slug"
-
   create_table "discovery_answers", :force => true do |t|
     t.string   "value"
     t.datetime "created_at", :null => false
@@ -96,13 +48,6 @@ ActiveRecord::Schema.define(:version => 20150126155114) do
 
   add_index "discovery_personalities", ["name"], :name => "index_discovery_personalities_on_name"
 
-  create_table "discovery_personality_answers", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "discovery_answer_id"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
-  end
-
   create_table "discovery_questions", :force => true do |t|
     t.string   "question"
     t.string   "option_1"
@@ -125,117 +70,5 @@ ActiveRecord::Schema.define(:version => 20150126155114) do
   end
 
   add_index "discovery_strengths", ["category_id"], :name => "index_discovery_strengths_on_category_id"
-
-  create_table "likes", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "major_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "majors", :force => true do |t|
-    t.text     "glance"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "name"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.string   "slug"
-    t.text     "usually_like"
-    t.text     "skills"
-    t.text     "shouldnt_pick"
-    t.integer  "salary"
-    t.text     "salary_description"
-    t.string   "salary_source"
-    t.text     "you_will_learn"
-    t.text     "outcomes_jobs"
-    t.text     "get_in_program"
-    t.text     "how_to_succeed"
-    t.text     "courses"
-    t.text     "more_info"
-  end
-
-  add_index "majors", ["name"], :name => "index_majors_on_name"
-  add_index "majors", ["slug"], :name => "index_majors_on_slug"
-
-  create_table "pg_search_documents", :force => true do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "prospects", :force => true do |t|
-    t.integer  "major_id"
-    t.integer  "career_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "reviews", :force => true do |t|
-    t.text     "review"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.integer  "user_id"
-    t.integer  "reviewable_id"
-    t.string   "reviewable_type"
-  end
-
-  add_index "reviews", ["user_id"], :name => "index_reviews_on_user_id"
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       :limit => 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "profile_name"
-    t.string   "ei"
-    t.string   "si"
-    t.string   "tf"
-    t.string   "jp"
-    t.string   "role"
-    t.string   "personality_type"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "votes", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "review_id"
-    t.boolean  "up"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
